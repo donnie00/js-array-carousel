3e365743846514327621a1d7b44428f0974e0cc6
ippoRulez <ippolitodonato@gmail.com>
2022-11-05 16:59:30 +0100

    added bonus-1 files

---
+++ bonus/bonus-1.js
@@ -0,0 +1,57 @@
+const imgs = [
+	'img/01.webp',
+	'img/02.webp',
+	'img/03.webp',
+	'img/04.webp',
+	'img/05.webp',
+];
+
+const imgContainerEl = document.getElementById('imgContainer');
+const upBtnEl = document.getElementById('upBtn');
+const downBtnEl = document.getElementById('downBtn');
+
+let activeImgIndex = 0;
+
+for (let i = 0; i < imgs.length; i++) {
+	const imgToAdd = document.createElement('img');
+
+	imgToAdd.src = imgs[i];
+
+	imgContainerEl.append(imgToAdd);
+
+	imgContainerEl.firstChild.classList.add('active');
+}
+
+const imgList = imgContainerEl.childNodes;
+
+upBtnEl.addEventListener('click', function () {
+	if (activeImgIndex <= 0) {
+		imgList[activeImgIndex].classList.remove('active');
+
+		activeImgIndex = imgList.length - 1;
+
+		imgList[activeImgIndex].classList.add('active');
+	} else {
+		imgList[activeImgIndex].classList.remove('active');
+
+		activeImgIndex--;
+
+		imgList[activeImgIndex].classList.add('active');
+	}
+});
+
+downBtnEl.addEventListener('click', function () {
+	if (activeImgIndex >= imgs.length - 1) {
+		imgList[activeImgIndex].classList.remove('active');
+
+		activeImgIndex = 0;
+
+		imgList[activeImgIndex].classList.add('active');
+	} else {
+		imgList[activeImgIndex].classList.remove('active');
+
+		activeImgIndex++;
+
+		imgList[activeImgIndex].classList.add('active');
+	}
+});
